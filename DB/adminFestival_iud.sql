c7efb067e554bef4f7b152711c3ffd17b25c1da9
juna5157 <wjenny237@daum.net>
2026-03-19 12:44:26 +0900

    나의 문의 상세조회

--- DB/adminFestival_iud.sql
+++ DB/adminFestival_iud.sql
@@ -114,11 +114,7 @@ SELECT SEQ_PLACE.NEXTVAL FROM DUAL;
 -- SEQ_PLACE.NEXTVAL이 현재 PLACE 테이블이의 최대 place_id가 동일한 크기가 아닐 때 적용
 DROP SEQUENCE SEQ_PLACE;
 CREATE SEQUENCE SEQ_PLACE
-<<<<<<< HEAD
-START WITH 1004 -- 최대 place_id + 1 (이거 상수만 넣을 수 있음. 주의!)
-=======
 START WITH 3551927 -- 최대 place_id + 1 (이거 상수만 넣을 수 있음. 주의!)
->>>>>>> dev
 INCREMENT BY 1;
 
 -- SEQ_TICKET의 최댓값 확인

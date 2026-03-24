a572821a7ab98e5122e64d0bf86e7137aa4627f4
youngeunsong <yesongee93@gmail.com>
2026-03-24 21:02:35 +0900

    특정 기간 페이징 처리 표 조회 쿼리 완성

--- DB/IUD/adminSurvey_iud.sql
+++ DB/IUD/adminSurvey_iud.sql
@@ -8,3 +8,45 @@ DROP SEQUENCE SEQ_SURVEY;
 
 SELECT max(survey_ID) FROM survey; 
 CREATE SEQUENCE SEQ_SURVEY START WITH 257 INCREMENT BY 1;
+
+-- 특정 기간 내의 survey 데이터 표로 열람 (페이징 처리)
+ SELECT  	survey_id, 
+ 			user_id, 
+	       reservation_id, 
+	       nps_score, 
+	       satisfaction_score, 
+	       inconvenience, 
+	       info_reliability_score, 
+	       improvements, 
+	       created_at
+  FROM (
+        SELECT ROW_NUMBER() OVER (ORDER BY CREATED_AT, survey_id DESC) AS rn,
+               survey_id,
+               user_id, 
+               reservation_id, 
+               nps_score, 
+               satisfaction_score, 
+               inconvenience, 
+               info_reliability_score, 
+               improvements, 
+               created_at      
+		  FROM SURVEY
+		 WHERE CREATED_AT >= '2026-03-01 00:00:00' -- 당일 데이터 조회 : TRUNC(SYSDATE), TRUNC(SYSDATE) + 1
+		   AND CREATED_AT < '2026-03-24 23:59:59' 
+		 )
+ WHERE rn BETWEEN 1 AND 10;
+
+
+-- 금일 설문 응답 결과 표로 열람
+SELECT * 
+  FROM SURVEY
+ WHERE CREATED_AT >= TRUNC(SYSDATE) 
+   AND CREATED_AT < TRUNC(SYSDATE) + 1
+ ORDER BY CREATED_AT, survey_id;
+
+-- 특정 기간 내의 survey 데이터 표로 열람
+SELECT * 
+  FROM SURVEY
+ WHERE CREATED_AT >= '2026-03-01 00:00:00' 
+   AND CREATED_AT < '2026-03-24 23:59:59'
+ ORDER BY CREATED_AT, survey_id;
